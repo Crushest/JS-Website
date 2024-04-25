e695ca701237f8109f81fa72d07b041556dd53e4
Crushest <sebi.estrada2k@gmail.com>
2024-04-25 11:07:14 -0700

    Adding Firebase for user Auth

---
+++ recipe.js
@@ -0,0 +1,13 @@
+//Firebase Setup Start
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMx5Txlm2VbxLUKL-xsJVMEvvJ2YcZSoc",
+    authDomain: "familycookbook-580bf.firebaseapp.com",
+    projectId: "familycookbook-580bf",
+    storageBucket: "familycookbook-580bf.appspot.com",
+    messagingSenderId: "365501345761",
+    appId: "1:365501345761:web:9bd74d44cd7a458a156ca0"
+  };
+
+  const app = initializeApp(firebaseConfig);
