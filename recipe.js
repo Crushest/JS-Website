da48bc49a57bc070c11486842e817c078600d0d4
Crushest <sebi.estrada2k@gmail.com>
2024-04-25 12:12:30 -0700

    Dropdown

--- recipe.js
+++ recipe.js
@@ -1,5 +1,11 @@
 //Firebase Setup Start
 import { initializeApp } from "firebase/app"
+import { getAuth,
+         sendEmailVerification,
+         createUserWithEmailAndPassword,
+         signInWithEmailAndPassword,
+         signOut, 
+          } from "firebase/auth"
 
 const firebaseConfig = {
     apiKey: "AIzaSyAMx5Txlm2VbxLUKL-xsJVMEvvJ2YcZSoc",
@@ -10,4 +16,71 @@ const firebaseConfig = {
     appId: "1:365501345761:web:9bd74d44cd7a458a156ca0"
   };
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+//Firebase Setup End
+
+function authCreateAccountWithEmail() {
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+createUserWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+  showLoggedInView()
+  clearAuthFields()
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  console.log(errorMessage)
+})};
+
+function authSignInWithEmail() {
+  const email = emailInputEl.value
+  const password = passwordInputEl.value
+  
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        showLoggedInView()
+        clearAuthFields()
+    })
+    .catch((error) => {
+        console.error(error.message)
+    })
+}
+
+function authSignOut() {
+  signOut(auth)
+      .then(() => {
+          showLoggedOutView()
+      }).catch((error) => {
+          console.error(error.message)
+      })
+}
+
+function clearInputField(field) {
+	field.value = ""
+}
+
+function clearAuthFields() {
+	clearInputField(emailInputEl)
+	clearInputField(passwordInputEl)
+}
+
+function showLoggedOutView() {
+  hideView(viewLoggedIn)
+  showView(viewLoggedOut)
+}
+
+function showLoggedInView() {
+  hideView(viewLoggedOut)
+  showView(viewLoggedIn)
+}
+
+function showView(view) {
+  view.style.display = "flex"
+}
+
+function hideView(view) {
+  view.style.display = "none"
+}
