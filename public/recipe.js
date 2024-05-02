b77c2ba2fb656aae43fc875b7532faaaf909345c
Crushest <sebi.estrada2k@gmail.com>
2024-05-02 12:48:31 -0700

    update

--- public/recipe.js
+++ public/recipe.js
@@ -10,13 +10,13 @@ import { getAuth,
          signInWithPopup } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMx5Txlm2VbxLUKL-xsJVMEvvJ2YcZSoc",
-    authDomain: "familycookbook-580bf.firebaseapp.com",
-    projectId: "familycookbook-580bf",
-    storageBucket: "familycookbook-580bf.appspot.com",
-    messagingSenderId: "365501345761",
-    appId: "1:365501345761:web:9bd74d44cd7a458a156ca0"
-  };
+apiKey: "AIzaSyAMx5Txlm2VbxLUKL-xsJVMEvvJ2YcZSoc",
+authDomain: "familycookbook-580bf.firebaseapp.com",
+projectId: "familycookbook-580bf",
+storageBucket: "familycookbook-580bf.appspot.com",
+messagingSenderId: "365501345761",
+appId: "1:365501345761:web:9bd74d44cd7a458a156ca0"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
